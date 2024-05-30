971ae6caf092fefa43344e2584f2b367b67f1e6d
Luis Castro <luismcastro99@gmail.com>
2024-05-30 20:48:41 -0300

    feat(colaborador) : Creado Model Imagen

--- proyecto/colaborador/models.py
+++ proyecto/colaborador/models.py
@@ -8,16 +8,28 @@ class Colaborador(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE, related_name="Colaborador")
     edad = models.PositiveIntegerField(max_length=2)
     GENEROS = [("M", "Mujer"), ("H", "Hombre"), ("NB", "No Binario")]
-    genero = models.CharField(max_length=1, choices=GENEROS, default="Hombre")
+    genero = models.CharField(choices=GENEROS, default="Hombre")
     foto_perfil = models.ImageField(upload_to="fotos_perfil", null=True, blank=True)
-    descripcion = models.TextField(blank=False, null=False)
+    biografia = models.TextField(blank=False, null=False)
     fecha_registro = models.DateTimeField(editable=False, default=timezone.now)
 
+    def __str__(self):
+        return f"{self.usuario.username}."
 
-def __str__(self):
-    return f"{self.usuario.username}, {self.edad} años, {self.genero}."
+    def clean(self):
+        if self.edad < 18:
+            raise ValidationError("Usted no puede ser menor de 18 años")
 
 
-def clean(self):
-    if self.edad < 18:
-        raise ValidationError("Usted no puede ser menor de 18 años")
+class Imagen(models.Model):
+    colaborador = models.ForeignKey(Colaborador, on_delete=models.SET_NULL, null=True, blank=True)
+    imagen = models.ImageField(upload_to="imagenes", null=True, blank=True)
+    titulo = models.CharField(max_length=50)
+    descripcion = models.TextField(blank=False, null=False)
+    fecha_subida = models.DateTimeField(editable=False, default=timezone.now)
+
+    class Meta:
+        ordering = "fecha_subida"
+
+    def __str__(self):
+        return f"{self.titulo}, by {self.colaborador}"
