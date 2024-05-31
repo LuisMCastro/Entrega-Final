ea97cfa3afaf6277607f65a30be57d8184b7720b
Luis Castro <luismcastro99@gmail.com>
2024-05-31 15:28:40 -0300

    feat(colaborador): urls

--- proyecto/colaborador/urls.py
+++ proyecto/colaborador/urls.py
@@ -6,6 +6,11 @@ from colaborador.views import (
     ColaboradorDetail,
     ColaboradorList,
     ColaboradorUpdate,
+    ImagenCreate,
+    ImagenDelete,
+    ImagenDetail,
+    ImagenList,
+    ImagenUpdate,
 )
 
 app_name = "colaborador"
@@ -16,4 +21,12 @@ urlpatterns = [
     path("colaborador/detail/<int:pk>", ColaboradorDetail.as_view(), name="colaborador_detail"),
     path("colaborador/update/<int:pk>", ColaboradorUpdate.as_view(), name="colaborador_update"),
     path("colaborador/delete/<int:pk>", ColaboradorDelete.as_view(), name="colaborador_delete"),
+]
+
+urlpatterns = [
+    path("imagen/list", ImagenList.as_view(), name="imagen_list"),
+    path("imagen/create", ImagenCreate.as_view(), name="imagen_create"),
+    path("imagen/detail/<int:pk>", ImagenDetail.as_view(), name="imagen_detail"),
+    path("imagen/update/<int:pk>", ImagenUpdate.as_view(), name="imagen_update"),
+    path("imagen/delete/<int:pk>", ImagenDelete.as_view(), name="imagen_delete"),
 ]
